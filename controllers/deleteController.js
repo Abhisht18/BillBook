bd3139b6514f0345ddb553a6e1c2c8fbe1e380cb
Abhisht18 <84927177+Abhisht18@users.noreply.github.com>
2021-08-10 15:24:22 +0530

    Add files via upload

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -15,6 +15,51 @@ const deleteItem = async(req, res) => {
         })
 }
 
+const deleteClient = async(req, res) => {
+    const id = req.params.id;
+    Client.find()
+        .then(clientData => {
+            const deleted = clientData[0].clients.filter(client => client._id != id); 
+            clientData[0].clients = [...deleted];
+            clientData[0].save();
+            return res.json({data: clientData[0].clients});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+const deleteInvoice = async(req, res) => {
+    const id = req.params.id;
+    Client.find()
+        .then(clientData => {
+            const deleted = clientData[0].sales_invoice.filter(invoice => invoice._id != id); 
+            clientData[0].sales_invoice = [...deleted];
+            clientData[0].save();
+            return res.json({data: clientData[0].sales_invoice});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+const deleteTransaction = async(req, res) => {
+    const id = req.params.id;
+    Client.find()
+        .then(clientData => {
+            const deleted = clientData[0].transactions.filter(transaction => transaction._id != id); 
+            clientData[0].transactions = [...deleted];
+            clientData[0].save();
+            return res.json({data: clientData[0].transactions});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
 module.exports = {
     deleteItem,
+    deleteClient,
+    deleteTransaction,
+    deleteInvoice
 }
